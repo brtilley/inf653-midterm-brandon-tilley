86d5fc694182ef898fcf4602141ddf536f77bcc5
Brandon Tilley <brtilley@mail.fhsu.edu>
2025-03-15 18:48:30 -0700

    Updating index.php file

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+echo '<pre>';
+print_r(getenv('SITE_URL'));
+echo '<br>';
+print_r($_SERVER);
+echo '</pre>';
+
+phpinfo();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
