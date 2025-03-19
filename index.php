35ba60a15def325b6818cc58b752abef91be4a28
Brandon Tilley <brtilley@mail.fhsu.edu>
2025-03-18 22:31:49 -0700

    removed header from API index.php

--- index.php
+++ index.php
@@ -8,17 +8,6 @@
 <body>
     <h1>Brandon Tilley - INF653 - Midterm Project</h1>
     <?php
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
-    $uri = $_SERVER['REQUEST_URI'];
-
-
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-        exit();
-    }
     
 echo '<pre>';
 print_r(getenv('https://inf653-midterm-brandon-tilley.onrender.com/'));
