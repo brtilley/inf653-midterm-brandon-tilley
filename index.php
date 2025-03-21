6761bfeb824e08d7967bc15d55290e54bb936562
Brandon Tilley <brtilley@mail.fhsu.edu>
2025-03-20 19:46:33 -0700

    updates

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <h1>Brandon Tilley - INF653 - Midterm Project</h1>
+
+<?php
+
+declare(strict_types = 1);
+
+echo '<pre>';
+print_r(getenv('SITE_URL'));
+echo '<br>';
+print_r($_SERVER);
+echo '</pre>';
+
+phpinfo();
