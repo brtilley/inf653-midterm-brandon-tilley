362973f07fea50a25347e7047dee8e3a18e82085
Brandon Tilley <brtilley@mail.fhsu.edu>
2025-03-21 19:39:00 -0700

    updating quotes.php

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -23,7 +23,9 @@
   $quote_arr = array(
     'id' => $quote->id,
     'quote' => $quote->quote,
+    'author_id' => $quote->author_id,
     'author' => $quote->author,
+    'category_id' => $quote->category_id,
     'category' => $quote->category
   );
 
