15045f3fbe330263ba8ecd2cd160497ea067395a
Brandon Tilley <brtilley@mail.fhsu.edu>
2025-03-25 21:41:57 -0700

    update 9:41

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -13,26 +13,57 @@
   $database = new Database();
   $db = $database->connect();
 
-  // Instantiate quote object
-  $quote = new Quote($db);
+    //Instantiate blog quote object
+    $quo = new Quote($db);
 
-  // Get raw quote data
-  $data = json_decode(file_get_contents("php://input"));
+    //Get the raw posted data
+    $data = json_decode(file_get_contents("php://input"));
 
-  // Set ID to update
+    //data is not set
+    if(!isset($data->id) || !isset($data->quote) || !isset($data->author_id) || !isset($data->category_id)){
+        echo json_encode(array('message' => 'Missing Required Parameters'));
+        exit();
+    }
+    //SET ID TO UPDATE
+    $quo->id = $data->id;
+    $quo->quote = $data->quote;
+    $quo->author_id = $data->author_id;
+    $quo->category_id = $data->category_id;
 
-  $quote->id = $data->id;
-  $quote->quote = $data->quote;
-  $quote->author_id = $data->author_id;
-  $quote->category_id = $data->category_id;
+    $aut = new Author($db);
+    $cat = new Category($db);
+    $aut->id = $quo->author_id;
+    $cat->id = $quo->category_id;
 
-  // Update quote
-  if($quote->update()) {
-    echo json_encode(
-      array('message' => 'Quote Updated')
-    );
-  } else {
-    echo json_encode(
-      array('message' => 'Quote Not Updated')
-    );
-  }
+    //conduct checks on category and author ids
+    $cat->read_single();
+    if(!$cat->category){
+        echo json_encode(array(
+            'message' => 'category_id Not Found'
+        ));
+        exit();
+    }
+
+    $aut->read_single();
+    if(!$aut->author){
+        echo json_encode(array(
+            'message' => 'author_id Not Found'
+        ));
+        exit();
+    }
+
+    //update quotes
+    if($quo->update()){
+        echo json_encode(
+            array(
+                'id' => $quo->id,
+                'quote' => $quo->quote,
+                'author_id' => $quo->author_id,
+                'category_id' => $quo->category_id
+        ));
+    } else {
+        echo json_encode(
+            array(
+                'message' => 'No Quotes Found'
+        ));
+    }
